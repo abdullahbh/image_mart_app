593316180682c46b9ef2d05277b1629bee384009
Aeiman191 <aimteiyaz191@gmail.com>
2024-03-11 14:24:42 +0500

    Cropping tool in image_viewer

--- lib/ui/image_veiwer.dart
+++ lib/ui/image_veiwer.dart
@@ -1,12 +1,44 @@
-// image_viewer.dart
-
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 class ImageViewerScreen extends StatelessWidget {
   final String imagePath;
 
-  const ImageViewerScreen({Key? key, required this.imagePath}) : super(key: key);
+  const ImageViewerScreen({Key? key, required this.imagePath})
+      : super(key: key);
+
+  Future<void> _cropImage() async {
+    ImageCropper imageCropper =
+        ImageCropper(); // Create an instance of ImageCropper
+    File? croppedImage = await imageCropper.cropImage(
+      sourcePath: File(imagePath).path,
+      maxWidth: 512,
+      maxHeight: 512,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9,
+      ],
+      androidUiSettings: AndroidUiSettings(
+        toolbarTitle: 'Crop Image',
+        toolbarColor: Colors.deepOrange,
+        toolbarWidgetColor: Colors.white,
+        initAspectRatio: CropAspectRatioPreset.original,
+        lockAspectRatio: false,
+      ),
+      iosUiSettings: IOSUiSettings(
+        title: 'Crop Image',
+      ),
+    );
+
+    if (croppedImage != null) {
+      // Do something with the cropped image
+      print('Cropped image path: ${croppedImage.path}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +46,34 @@ class ImageViewerScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Image Viewer"),
       ),
-      body: Center(
-        child: Image.file(
-          File(imagePath),
-          fit: BoxFit.cover,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.file(
+                File(imagePath),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(height: 16.0), // Adjust the spacing as needed
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: _cropImage,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.crop),
+                  SizedBox(width: 8.0),
+                  Text("Crop Image"),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
